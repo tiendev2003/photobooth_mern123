001c8392b4ac09f61181777dba54ae847e39b68c
tiendev2003 <tientc.21it@vku.udn.vn>
2025-07-29 00:08:22 +0700

    up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import GIF from "gif.js";
 import { twMerge } from "tailwind-merge";
 
-export const TIMEOUT_DURATION = 10; // 10 seconds for normal video generation 
+export const TIMEOUT_DURATION = 2; // 10 seconds for normal video generation 
 
 
 export function cn(...inputs: ClassValue[]) {
